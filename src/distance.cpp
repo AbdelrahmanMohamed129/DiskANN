944e355df80b0af8a9d29776bf44b9c2e8210568
AliHashish <Ali.Hashish01@eng-st.cu.edu.eg>
2023-10-27 20:40:16 +0200

    error

--- src/distance.cpp
+++ src/distance.cpp
@@ -586,6 +586,7 @@ float AVXDistanceInnerProductFloat::compare(const float *a, const float *b, uint
     // std::cout << "-result: " << -result << std::endl;
     // 5odo b probability ( ( el cache size / el 10^14 ) * 2 ) mthln aw 5% mthln
     std::cout << "cache size: " << dist_cache.size() << std::endl;
+    lolxD
     if (dist_cache.size() < (1e13))
         dist_cache.emplace(std::make_pair(std::make_pair(std::make_pair(a[0],a[1]),std::make_pair(b[0],b[1])), - result));
         
